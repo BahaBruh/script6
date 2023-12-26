3f76b6cb207a278e16fd9303f8c7c36d250305f5
baha <baha@gmail.com>
2023-12-26 15:44:14 +0500

    first

---
+++ script.js
@@ -0,0 +1,38 @@
+const users = {
+    
+}
+
+
+    for (var i = 1; i < 6; i++) {
+        
+        users[i] = {
+        name: 'Имя : ' + prompt('Введите имя'),
+        age:  'Возраст : ' + +prompt(' Введите возраст')
+        }
+        
+    }
+    for (const key in users) {
+        console.log( ` Пользователь - ${i} : `);
+        for (const key2 in users[key]) {
+            console.log(users[key][key2]);
+        }
+    }
+
+
+
+
+
+const rec = receipt()
+
+console.log(rec);
+
+for (const key in rec) {
+        console.log( ` Вы заказали : ${key} `);
+    console.log(` info : ${rec[key].info}`);
+    console.log(` price : ${rec[key].price}`);
+    console.log( 'Общая стоимость с учетом доставки - 9000 =  ' + rec[key].price + rec[key].price + 9000 );
+        }
+        
+
+    
+    
